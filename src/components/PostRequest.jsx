f9ed5072bae4a4eb46f2616c96249bc903ce4f29
jordonss <nemezg@gmail.com>
2024-01-15 10:45:18 +0200

    PostRequest

--- src/components/PostRequest.jsx
+++ src/components/PostRequest.jsx
@@ -48,6 +48,7 @@ export default function PostRequest() {
 						<input type='file' placeholder='Upload your photo' />
 					</div>
 					<Button>Sign up</Button>
+					<Button>Sign up</Button>
         </form>
       </div>
     </div>
