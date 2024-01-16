e25d93048cc1ed1a846af8528c72e655939db8c6
jordonss <nemezg@gmail.com>
2024-01-16 12:05:10 +0200

    radio

--- src/components/PostRequest.jsx
+++ src/components/PostRequest.jsx
@@ -22,32 +22,36 @@ export default function PostRequest() {
     <div className={classes.wrapper}>
       <div className={classes.postSection}>
         <h2>Working with POST request</h2>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='name'>Name</label>
-            <input type='name' id='name' required />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' required />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='phone'>Phone</label>
-            <input type='phone' id='phone' required />
-            <span>+38 (XXX) XXX-XX-XX</span>
-          </div>
-          <h3>Select your position</h3>
-          {positions.map((position) => (
-            <div key={position.id}>
-              <input type='radio' name='position' />
-              <label htmlFor='position'>{position.name}</label>
+        <form className={classes.postForm}>
+          <div className={classes.inputWrapper}>
+            <div className={classes.control}>
+              {/* <label htmlFor='name'>Name</label> */}
+              <input type='name' id='name' required placeholder='Your name' />
             </div>
-          ))}
-					<div>
-						<button type='submit'>Upload</button>
-						<input type='file' placeholder='Upload your photo' />
-					</div>
-					<Button>Sign up</Button>
+            <div className={classes.control}>
+              {/* <label htmlFor='email'>Email</label> */}
+              <input type='email' id='email' required placeholder='Email' />
+            </div>
+            <div className={classes.control}>
+              <input type='phone' id='phone' required placeholder='Phone' />
+              {/* <span className={classes.number}>+38 (XXX) XXX-XX-XX</span> */}
+              <label htmlFor='phone'>+38 (XXX) XXX-XX-XX</label>
+            </div>
+          </div>
+          <div className={classes.radio}>
+            <h3>Select your position</h3>
+            {positions.map((position) => (
+              <div className={classes.radioPosition} key={position.id}>
+                <input type='radio' name='position' />
+                <label htmlFor='position' className={classes.radioLabel}>{position.name}</label>
+              </div>
+            ))}
+          </div>
+          <div>
+            <button type='submit'>Upload</button>
+            <input type='file' placeholder='Upload your photo' />
+          </div>
+          <Button>Sign up</Button>
         </form>
       </div>
     </div>
