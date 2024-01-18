56d30040fd3f05c599df8beb463035d79be24a1a
jordonss <nemezg@gmail.com>
2024-01-18 11:00:00 +0200

    token

--- src/components/PostRequest.jsx
+++ src/components/PostRequest.jsx
@@ -4,6 +4,70 @@ import Button from './ui/Button';
 
 export default function PostRequest() {
   const [positions, setPositions] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [position, setPosition] = useState('');
+  const [token, setToken] = useState('');
+
+	useEffect(() => {
+    // Fetch token when the component mounts
+    const fetchToken = async () => {
+      try {
+        const tokenResponse = await fetch('https://frontend-test-assignment-api.abz.agency/api/v1/token ', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (tokenResponse.ok) {
+          const tokenData = await tokenResponse.json();
+          setToken(tokenData.token);
+        } else {
+          console.error('Error fetching token:', tokenResponse.statusText);
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
+      }
+    };
+		console.log(token)
+    fetchToken();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      name,
+      email,
+      phone,
+      position,
+    };
+
+    console.log(formData);
+
+    try {
+      const response = await fetch(
+        'https://frontend-test-assignment-api.abz.agency/api/v1/users',
+        {
+          method: 'POST',
+					body: formData,
+          headers: {
+            'Token': token,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+			if(response.ok) {
+				const responseData = await response.json()
+				console.log(responseData);
+			}
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const fetchPosition = async () => {
@@ -22,19 +86,38 @@ export default function PostRequest() {
     <div className={classes.wrapper}>
       <div className={classes.postSection}>
         <h2>Working with POST request</h2>
-        <form className={classes.postForm}>
+        <form onSubmit={handleSubmit} className={classes.postForm}>
           <div className={classes.inputWrapper}>
             <div className={classes.control}>
-              {/* <label htmlFor='name'>Name</label> */}
-              <input type='name' id='name' required placeholder='Your name' />
+              <input
+                type='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id='name'
+                required
+                placeholder='Your name'
+              />
             </div>
             <div className={classes.control}>
               {/* <label htmlFor='email'>Email</label> */}
-              <input type='email' id='email' required placeholder='Email' />
+              <input
+                type='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                id='email'
+                required
+                placeholder='Email'
+              />
             </div>
             <div className={classes.control}>
-              <input type='phone' id='phone' required placeholder='Phone' />
-              {/* <span className={classes.number}>+38 (XXX) XXX-XX-XX</span> */}
+              <input
+                type='phone'
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                id='phone'
+                required
+                placeholder='Phone'
+              />
               <label htmlFor='phone'>+38 (XXX) XXX-XX-XX</label>
             </div>
           </div>
@@ -42,8 +125,16 @@ export default function PostRequest() {
             <h3>Select your position</h3>
             {positions.map((position) => (
               <div className={classes.radioPosition} key={position.id}>
-                <input type='radio' name='position' />
-                <label htmlFor='position' className={classes.radioLabel}>{position.name}</label>
+                <input
+                  type='radio'
+                  value={position.name}
+                  onChange={(e) => setPosition(e.target.value)}
+                  name='position'
+                  required
+                />
+                <label htmlFor='position' className={classes.radioLabel}>
+                  {position.name}
+                </label>
               </div>
             ))}
           </div>
@@ -51,7 +142,7 @@ export default function PostRequest() {
             <button type='submit'>Upload</button>
             <input type='file' placeholder='Upload your photo' />
           </div>
-          <Button>Sign up</Button>
+          <Button type='submit'>Sign up</Button>
         </form>
       </div>
     </div>
